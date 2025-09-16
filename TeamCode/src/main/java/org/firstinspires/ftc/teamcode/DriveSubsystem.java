65109f7b96e2f874dbad8e5e9a95fba97ff09658
ClaireO-Brien <claireobrienca@gmail.com>
2025-09-15 17:36:06 -0700

    created drivetrain using command base

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveSubsystem.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.seattlesolvers.solverslib.command.SubsystemBase;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class DriveSubsystem extends SubsystemBase {
+
+    private DcMotor frontRight;
+    private DcMotor frontLeft;
+    private DcMotor backRight;
+    private DcMotor backLeft;
+
+    public DriveSubsystem(final HardwareMap hMap, final String name) {
+        frontRight = hMap.get(DcMotor.class, "rightFront");
+        frontLeft = hMap.get(DcMotor.class, "leftFront");
+        backRight = hMap.get(DcMotor.class, "rightRear");
+        backLeft = hMap.get(DcMotor.class, "leftRear");
+
+        frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+
+    public void setDriveVectors(double x, double y, double rx) {
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        frontLeft.setPower((y + x + rx) / denominator);
+        backLeft.setPower((y - x + rx) / denominator);
+        frontRight.setPower((y - x - rx) / denominator);
+        backRight.setPower((y + x - rx) / denominator);
+    }
+
+
+}
