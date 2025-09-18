a9796e70d02431902ead69179dac57df836805ec
braydenwaffles <hobrayden@gmail.com>
2025-09-17 17:16:53 -0700

    please help me michael

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CrabCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CrabCommand.java
@@ -9,7 +9,8 @@ public class CrabCommand extends CommandBase {
     }
     public commandState state = commandState.OPEN;
     private final BraydenSubSystem claw;
-    private static ElapsedTime time = new ElapsedTime();
+    private ElapsedTime time = new ElapsedTime();
+    private ElapsedTime helpme = new ElapsedTime();
     public CrabCommand(BraydenSubSystem subSystem) {
         claw = subSystem;
         //add requirements
@@ -19,19 +20,20 @@ public class CrabCommand extends CommandBase {
     public void initialize() {
         claw.clawOpen();
         time.reset();
+        helpme.reset();
     }
 
     @Override
     public void execute() {
         switch (state) {
             case OPEN:
-                if (time.time() == 1.0) {
+                if (time.time() >= 0.4) {
                     state = commandState.CLOSE;
                     time.reset();
                 }
                 break;
             case CLOSE:
-                if (time.time() > 1.0) {
+                if (time.time() >= 0.4) {
                     state = commandState.OPEN;
                     time.reset();
                 }
@@ -41,11 +43,8 @@ public class CrabCommand extends CommandBase {
 
     @Override
     public boolean isFinished() {
-        if (time.time() < 10) {
-            return false;
-        } else {
-            return true;
-        }
+//        return helpme.time() < 5;
+        return false;
     }
 
     @Override
