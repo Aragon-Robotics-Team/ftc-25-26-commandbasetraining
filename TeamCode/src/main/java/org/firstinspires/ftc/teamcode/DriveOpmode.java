b710ebae2c6aced93001dde0a46719a411457787
ClaireO-Brien <claireobrienca@gmail.com>
2025-09-17 17:30:51 -0700

    fxied drive stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveOpmode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveOpmode.java
@@ -6,11 +6,12 @@ import com.seattlesolvers.solverslib.gamepad.GamepadKeys;
 
 public class DriveOpmode extends CommandOpMode {
     public GamepadEx driver1;
-    DriveSubsystem drive = new DriveSubsystem(hardwareMap, "drive");
+    DriveSubsystem drive;
 
     @Override
     public void initialize () {
         super.reset();
+        drive = new DriveSubsystem(hardwareMap, "drive");
         driver1 = new GamepadEx(gamepad1);
         driver1.getGamepadButton(GamepadKeys.Button.A).whenPressed(new DriveForwardCommand(drive));
         driver1 = new GamepadEx(gamepad1);
