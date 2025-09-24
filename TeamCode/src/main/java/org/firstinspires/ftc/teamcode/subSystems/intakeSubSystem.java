551e7c685b1b95c03fab94aae59be46ff2620546
braydenwaffles <hobrayden@gmail.com>
2025-09-24 15:24:38 -0700

    Update intakeSubSystem.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subSystems/intakeSubSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subSystems/intakeSubSystem.java
@@ -20,7 +20,10 @@ public class intakeSubSystem extends SubsystemBase { //intake subsystem setup
         EXTENDED, TRANSFER, TUCKED
     }
 
-    enum
+    enum IntakeState {
+        FORWARD, REVERSE, STOP
+    }
+
     public PivotState pivotState  = PivotState.TUCKED;
     private DcMotor extendo;
     private Servo intakeWrist;
@@ -28,9 +31,9 @@ public class intakeSubSystem extends SubsystemBase { //intake subsystem setup
     //methods: setPivot, setExtendoTarget, setIntake
 
     public intakeSubSystem(final HardwareMap hMap,final String name) {
-        intakeWrist = hMap.get(Servo.class, name);
+        intakeWrist = hMap.get(Servo.class, name); //do not to specify run using when servo
         flyWheel = hMap.get(DcMotor.class, name);
-        flyWheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        flyWheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER); // make sure to specify run mode when using DCMOTOR
         extendo = hMap.get(DcMotor.class, name);
         extendo.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
@@ -67,17 +70,31 @@ public class intakeSubSystem extends SubsystemBase { //intake subsystem setup
 
     }
 
-    public void setExtendo(ExtendoState state) {
+    public void setExtendoTarget(ExtendoState state) {
         switch (state) {
             case TUCKED:
-                extendo.setTargetPosition(1.0); //replace with PID + constants
+                extendo.setTargetPosition(1); //replace with PID + constants
                 break;
             case TRANSFER:
                 // add possible logic like IF already tucked then you don't need to change transfer
-                extendo.setTargetPosition(1.0); //replace with PID + constants
+                extendo.setTargetPosition(1); //replace with PID + constants
                 break;
             case EXTENDED:
-                extendo.setTargetPosition(1.0); //replace with PID + constants
+                extendo.setTargetPosition(1); //replace with PID + constants
+                break;
+        }
+    }
+
+    public void setIntake(IntakeState state) {
+        switch (state) {
+            case STOP:
+                flyWheel.setTargetPosition(0); //stopped
+                break;
+            case REVERSE:
+                flyWheel.setTargetPosition(0); //replace with constant or the # of rotations
+                break;
+            case FORWARD:
+                flyWheel.setTargetPosition(0); //replace with constant or the # of rotations
                 break;
         }
     }
